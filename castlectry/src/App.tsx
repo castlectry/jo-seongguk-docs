290096cf67d07d2f9b22326d1c9d24946535a78d
조성국 <ksturtle2@naver.com>
2025-10-11 22:20:37 +0900

    refactor: 파일명 변경 사항 적용, feat: section 적용에 따른 컴포넌트 추가

--- castlectry/src/App.tsx
+++ castlectry/src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 import Banner from "./components/banner/Banner.tsx";
-import { useGlobalScrollNavigation } from "./hooks/useScrollNavigation.tsx";
+import { useGlobalScrollNavigation } from "./hooks/UseScrollNavigation.tsx";
 import Header from "./components/layout/Header.tsx";
+import AboutMe from "./components/about/AboutMe.tsx";
+import Skills from "./components/skills/Skills.tsx";
+// import Projects from "./components/projects/Projects.tsx";
+import Career from "./components/carrer/Career.tsx";
 
 
 export default function App() {
@@ -10,32 +14,27 @@ export default function App() {
     const { scrollNext } = useGlobalScrollNavigation([
         '.section-banner',
         '.section-myinfo',
+        '.section-skills',
         // '.section-projects',
-        // '.section-contact',
+        '.section-career'
     ]);
 
     return (
         <>
             <Header />
+
             <div className="min-h-screen w-full bg-gradient-to-r from-blue-600 via-mint-600 to-pink-600 text-white">
                 {/* 배너 섹션 */}
                 <Banner checkWidth={false} scrollNext={scrollNext}/>
 
-                {/* 다음 섹션 (스크롤 테스트용) */}
-                <section
-                    className="section-myinfo h-screen flex flex-col items-center justify-center bg-gray-900 text-center"
-                >
-                    <h2 className="text-4xl font-bold mb-4">About Me</h2>
-                    <p className="max-w-xl text-gray-300">
-                        Banner에서 아래 화살표 버튼 클릭 시 스크롤되는 영역<br/>
-                    </p>
-                    {/* TODO: 모바일 환경 동적 랜더링 및 컴포넌트 구성
-                                헤더
-                                Skills
-                                Projects
-                                Carrer 컴포넌트 구성
-                      */}
-                </section>
+                <AboutMe />
+
+                <Skills />
+
+                {/*<Projects />*/}
+
+                <Career />
+
             </div>
         </>
     );
